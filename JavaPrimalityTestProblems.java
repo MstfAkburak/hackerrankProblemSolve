73c41126b001b9a07e713831142af31b21e2ae67
MstfAkburak <onlysecretgame@gmail.com>
2021-07-04 23:11:50 +0300

    javaPrimalityTest

---
+++ JavaPrimalityTestProblems.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class JavaPrimalityTestProblems {
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in);)
+        {
+            System.out.println(scanner.nextBigInteger().isProbablePrime(100) ? "prime" : "not prime");
+        }
+    }
+
+}
